b4f30f2796c021a798172c31e71c8ff5938b2e67
qitas <qitas@qitas.cn>
2021-03-15 17:26:58 +0800

    try

--- builder/frameworks/native.py
+++ builder/frameworks/native.py
@@ -14,36 +14,6 @@ FRAMEWORK_DIR = platform.get_package_dir("E13A")
 assert isdir(FRAMEWORK_DIR)
 
 
-def get_core_files():
-    command = [
-        env.subst("$CC"), "-m%s" % board_config.get("build.cpu"),
-        "-D%s" % board_config.get("build.mcu")[0:5].upper(),
-        "-I.", "-I", "%s" % env.subst("$PROJECTSRC_DIR"),
-        "-Wp-MM", "-E", "ch554.h"
-    ]
-
-    result = exec_command(
-        command,
-        cwd=join(FRAMEWORK_DIR,"inc"),
-        env=env['ENV']
-    )
-
-    if result['returncode'] != 0:
-        sys.stderr.write(
-            "Error: Could not parse library files for the target.\n")
-        sys.stderr.write(result['err'])
-        env.Exit(1)
-
-    src_files = []
-    includes = result['out']
-    for inc in includes.split(" "):
-        if "_" not in inc or ".h" not in inc :
-            continue
-        src_files.append(basename(inc).replace(".h", ".c").strip())
-
-    return src_files
-
-
 env.Append(
     CFLAGS=[
         "--model-small"
@@ -57,6 +27,5 @@ env.Append(
 
 env.BuildSources(
     join("$BUILD_DIR", "native"),
-    join(FRAMEWORK_DIR,"src"),
-    src_filter=["-<*>"] + [" +<%s>" % f for f in get_core_files()]
+    join(FRAMEWORK_DIR,"src")
 )
