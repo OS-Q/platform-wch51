e6b439cf1fd73218226a34713eb9d06f3d759e5e
qitas <qitas@qitas.cn>
2021-05-06 17:29:22 +0800

    fix

--- builder/main.py
+++ builder/main.py
@@ -1,11 +1,26 @@
+# Copyright 2014-present PlatformIO <contact@platformio.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 from os.path import join
-from platform import system
 
-from SCons.Script import (AlwaysBuild, COMMAND_LINE_TARGETS, Default,
-                            DefaultEnvironment)
+from SCons.Script import ARGUMENTS, AlwaysBuild, Default, DefaultEnvironment
+
 
 def __getSize(size_type, env):
+    # FIXME: i don't really know how to do this right. see:
+    #        https://community.platformio.org/t/missing-integers-in-board-extra-flags-in-board-json/821
     return str(env.BoardConfig().get("build", {
         # defaults
         "size_heap": 1024,
@@ -13,6 +28,27 @@ def __getSize(size_type, env):
         "size_xram": 65536,
         "size_code": 65536,
     })[size_type])
+
+
+def _parseSdccFlags(flags):
+    assert flags
+    if isinstance(flags, list):
+        flags = " ".join(flags)
+    flags = str(flags)
+    parsed_flags = []
+    unparsed_flags = []
+    prev_token = ""
+    for token in flags.split(" "):
+        if prev_token.startswith("--") and not token.startswith("-"):
+            parsed_flags.extend([prev_token, token])
+            prev_token = ""
+            continue
+        if prev_token:
+            unparsed_flags.append(prev_token)
+        prev_token = token
+    unparsed_flags.append(prev_token)
+    return (parsed_flags, unparsed_flags)
+
 
 env = DefaultEnvironment()
 board_config = env.BoardConfig()
@@ -24,65 +60,68 @@ env.Replace(
     LD="sdld",
     RANLIB="sdranlib",
     OBJCOPY="sdobjcopy",
-    WCHISP="wchisptool -g -f",
     OBJSUFFIX=".rel",
     LIBSUFFIX=".lib",
     SIZETOOL=join(env.PioPlatform().get_dir(), "builder", "size.py"),
-    CFLAGS=[
-        "-m%s" % board_config.get("build.cpu")
-    ],
-    CPPDEFINES=[
-        "F_CPU=$BOARD_F_CPU",
-        "HEAP_SIZE=" + __getSize("size_heap", env)
-    ],
-    LINKFLAGS=[
-        "-m%s" % board_config.get("build.cpu"),
-        "--nostdlib",
-        "--code-size", board_config.get("build.size_code"),
-        "--iram-size", board_config.get("build.size_iram"),
-        "--xram-size", board_config.get("build.size_xram"),
-        "--out-fmt-ihx"
-    ],
 
-    LIBPATH=[
-        join(env.PioPlatform().get_package_dir("toolchain-sdcc"),
-            "%s" % "lib" if system() == "Windows" else join("share", "sdcc", "lib"),
-            board_config.get("build.cpu"))
-    ],
-    SIZEPROGREGEXP=r"^(?:HOME|GSINIT|GSFINAL|CODE|INITIALIZER)\s+([0-9]+).*",
-    SIZEDATAREGEXP=r"^(?:DATA|INITIALIZED)\s+([0-9]+).*",
-    SIZEEEPROMREGEXP=r"^(?:EEPROM)\s+([0-9]+).*",
-    SIZECHECKCMD="$SIZETOOL -A $SOURCES",
-    SIZEPRINTCMD='$SIZETOOL -d $SOURCES',
+    SIZECHECKCMD='$PYTHONEXE $SIZETOOL $SOURCES',
+    SIZEPRINTCMD='"$PYTHONEXE" $SIZETOOL $SOURCES',
+    SIZEPROGREGEXP=r"^ROM/EPROM/FLASH\s+[a-fx\d]+\s+[a-fx\d]+\s+(\d+).*",
 
     PROGNAME="firmware",
     PROGSUFFIX=".hex"
 )
 
-# Allow user to override via pre:script
-if env.get("PROGNAME", "program") == "program":
-    env.Replace(PROGNAME="firmware")
-
 env.Append(
-    ASFLAGS=env.get("CFLAGS", [])[:]
+    ASFLAGS=env.get("CCFLAGS", [])[:],
+
+    CFLAGS=[
+        "--std-sdcc11"
+    ],
+
+    CCFLAGS=[
+        "--opt-code-size",  # optimize for size
+        "--peep-return",    # peephole optimization for return instructions
+        "-m%s" % board_config.get("build.cpu")
+    ],
+
+    CPPDEFINES=[
+        "F_CPU=$BOARD_F_CPU",
+        "HEAP_SIZE=" + __getSize("size_heap", env)
+    ],
+
+    LINKFLAGS=[
+        "-m%s" % board_config.get("build.cpu"),
+        "--iram-size", __getSize("size_iram", env),
+        "--xram-size", __getSize("size_xram", env),
+        "--code-size", __getSize("size_code", env),
+        "--out-fmt-ihx"
+    ]
 )
+
+if int(ARGUMENTS.get("PIOVERBOSE", 0)):
+    env.Prepend(UPLOADERFLAGS=["-v"])
+
+# parse manually SDCC flags
+if env.get("BUILD_FLAGS"):
+    _parsed, _unparsed = _parseSdccFlags(env.get("BUILD_FLAGS"))
+    env.Append(CCFLAGS=_parsed)
+    env['BUILD_FLAGS'] = _unparsed
+
+project_sdcc_flags = None
+if env.get("SRC_BUILD_FLAGS"):
+    project_sdcc_flags, _unparsed = _parseSdccFlags(env.get("SRC_BUILD_FLAGS"))
+    env['SRC_BUILD_FLAGS'] = _unparsed
 
 #
 # Target: Build executable and linkable firmware
 #
 
-target_elf = None
-if "nobuild" in COMMAND_LINE_TARGETS:
-    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
-    target_firm = join("$BUILD_DIR", "${PROGNAME}.ihx")
-else:
-    target_elf = env.BuildProgram()
-    target_firm = env.Command(
-        join("$BUILD_DIR", "${PROGNAME}.ihx"),
-        env['PIOBUILDFILES'],
-        env['LINKCOM'].replace("$LINKFLAGS", "$ldflags_for_ihx")
-    )
-    env.Depends(target_firm, target_elf)
+target_firm = env.BuildProgram()
+
+if project_sdcc_flags:
+    env.Import("projenv")
+    projenv.Append(CCFLAGS=project_sdcc_flags)
 
 AlwaysBuild(env.Alias("nobuild", target_firm))
 target_buildprog = env.Alias("buildprog", target_firm, target_firm)
@@ -90,11 +129,61 @@ target_buildprog = env.Alias("buildprog", target_firm, target_firm)
 #
 # Target: Print binary size
 #
+
 target_size = env.Alias(
     "size", target_firm,
     env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
 AlwaysBuild(target_size)
 
+#
+# Target: Upload firmware
+#
+
+upload_protocol = env.subst("$UPLOAD_PROTOCOL")
+upload_actions = []
+
+if upload_protocol == "stcgal":
+    f_cpu_khz = int(board_config.get("build.f_cpu")) / 1000
+    stcgal_protocol = board_config.get("upload.stcgal_protocol")
+    stcgal = join(env.PioPlatform().get_package_dir("tool-stcgal") or "", "stcgal.py")
+    env.Replace(
+        UPLOADERFLAGS=[
+            "-P", stcgal_protocol,
+            "-p", "$UPLOAD_PORT",
+            "-t", int(f_cpu_khz),
+            "-a"
+        ],
+        UPLOADCMD="python3 %s $UPLOADERFLAGS $SOURCE" % stcgal )
+
+    upload_actions = [
+        env.VerboseAction(env.AutodetectUploadPort,
+                          "Looking for upload port..."),
+        env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
+    ]
+
+# CH55x upload tool
+elif upload_protocol == "ch55x":
+    env.Replace(
+        UPLOADER="ch55xtool.py",
+        UPLOADERFLAGS=[
+            "-f"
+        ],
+        UPLOADCMD="python3 $UPLOADER $UPLOADERFLAGS $BUILD_DIR/${PROGNAME}.bin")
+
+    upload_actions = [
+        env.VerboseAction(" ".join(["$OBJCOPY","-I","ihex","-O","binary",
+            "$SOURCE", "$BUILD_DIR/${PROGNAME}.bin"]), "Creating binary"),
+        env.VerboseAction("$UPLOADCMD", "Uploading ${PROGNAME}.bin")
+    ]
+
+# custom upload tool
+elif upload_protocol == "custom":
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+else:
+    sys.stderr.write("Warning! Unknown upload protocol %s\n" % upload_protocol)
+
+AlwaysBuild(env.Alias("upload", target_firm, upload_actions))
 
 #
 # Setup default targets
